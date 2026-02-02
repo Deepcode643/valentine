1dd2a617bb95c09e7c238178457e78b7e50e2651
Deepcode643 <deepcodes643@gmail.com>
2026-02-02 23:35:23 +0530

    Create script.js

---
+++ css/script.js
@@ -0,0 +1,19 @@
+const yesBtn = document.getElementById("yesBtn");
+const noBtn = document.getElementById("noBtn");
+const container = document.getElementById("container");
+
+let yesSize = 1;
+
+noBtn.addEventListener("click", () => {
+  yesSize += 0.3;
+  yesBtn.style.transform = `scale(${yesSize})`;
+});
+
+yesBtn.addEventListener("click", () => {
+  container.innerHTML = `
+    <h1>I knew you’d say YES! 💖💖</h1>
+    <img src="https://media.giphy.com/media/26FLdmIp6wJr91JAI/giphy.gif" 
+         alt="Kissing Cartoon"
+         style="width:300px; margin-top:20px;">
+  `;
+});
